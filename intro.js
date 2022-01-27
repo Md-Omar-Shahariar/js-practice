82a7890241aea73b9758ba5f767e8a2dbdda92b5
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-01-27 18:01:38 +0600

    js

---
+++ intro.js
@@ -0,0 +1,18 @@
+// var ad = 67;
+// var hug = 67;
+// var a = ad+hug;
+// console.log(typeof( parseFloat( parseInt((a.toFixed()))).toFixed));
+
+// var friendAge = [12,23,345,56];
+// var oddNumbers = [1,3,5,7,9];
+// var vowels = ['a','e','i','o','u'];
+// console.log(vowels.length);
+// console.log(oddNumbers.indexOf(7));
+// console.log(typeof(friendAge));
+
+// var bookPages = ['hablu','bolod','choddogusti','peracitamol'];
+// console.log(bookPages.indexOf("peracitamol"))
+var array = ['asd','sadj','sdsa','sadbbu'];
+array.push('adsdgh', "fh");
+array.pop();
+console.log(array);
