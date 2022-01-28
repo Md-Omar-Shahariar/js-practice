924a234408ffcfd65841e781fc6aeac41176afa3
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-01-29 01:23:43 +0600

    js

---
+++ function.js
@@ -0,0 +1,26 @@
+// function StartFan(){
+//     console.log("walk toword Switch");
+//     console.log("Press The Switch");
+
+// }
+
+// var i=0;
+// while(true){
+//     var k =0;
+//     for ( k=0; k<i ;k++){
+        
+        
+//     }
+//     if(i>7){
+//         break;
+//     }
+//     i++;
+
+// }
+function addNumber(number1, number2) {
+    return "Result is " + number1 + number2;
+   }
+   
+   var result = addNumber(5, 4);
+   console.log(typeof(result));
+
